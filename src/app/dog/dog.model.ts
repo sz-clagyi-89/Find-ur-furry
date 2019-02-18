cff44d4aa472c8584025b78900932617cdf548d2
Szabolcs Szilágyi <sz.clagyi.89@gmail.com>
2019-02-18 16:51:01 +0100

    iterating instances into list and item comps

--- src/app/dog/dog.model.ts
+++ src/app/dog/dog.model.ts
@@ -3,13 +3,15 @@ export class Dog {
     public bread: string;
     public personality: string;
     public size: string;
+    public imagePath: string;
     public vaccination: string[];
 
-    constructor(name: string, bread: string, pers: string, size: string, vacc: string[]) {
+    constructor(name: string, bread: string, pers: string, size: string, img: string, vacc: string[]) {
         this.name = name;
         this.bread = bread;
         this.personality = pers;
         this.size = size;
+        this.imagePath = img;
         this.vaccination = vacc;
     }
 }
--- src/app/dog/dog.service.ts
+++ src/app/dog/dog.service.ts
@@ -7,6 +7,7 @@ export class DogService {
             'Daschund mix',
             'Cheeky',
             'Small',
+            'https://media.defense.gov/2012/Oct/24/2000103190/780/780/0/121022-F-PM370-008.JPG',
             [ 'parvo', 'hydrophobia' ]
         ),
         new Dog(
@@ -14,6 +15,7 @@ export class DogService {
             'Daschund mix',
             'playfull',
             'Small',
+            'https://get.pxhere.com/photo/dog-fire-mammal-black-hound-domestic-animal-vertebrate-dog-breed-dachshund-guard-dog-small-dog-dog-like-mammal-carnivoran-dobermann-austrian-black-and-tan-hound-polish-hunting-dog-transylvanian-hound-black-and-tan-coonhound-1384458.jpg',
             [ 'parvo', 'hydrophobia', 'vermifuge' ]
         ),
         new Dog(
@@ -21,6 +23,7 @@ export class DogService {
             'Poodle',
             'Chill',
             'Medium',
+            'https://cdn.pixabay.com/photo/2013/11/28/12/14/dog-220457_960_720.jpg',
             [ 'parvo', 'hydrophobia', 'vermifuge' ]
         )
     ];
